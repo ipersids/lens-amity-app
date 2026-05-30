83b85ca31b7d303088c6a4b2f71abb852f81e2a4
Julia Llama <julia.persidskaia@gmail.com>
2026-05-30 16:51:26 +0300

    feat(server): add gracefull shutdown

--- server/main.go
+++ server/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"errors"
-	"fmt"
+	"context"
 	"lensamity/internal/handler"
 	"lensamity/internal/middleware"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -29,13 +30,26 @@ func main() {
 		IdleTimeout:  120 * time.Second,
 	}
 
-	slog.Info("server starting", "port", server.Addr)
-	err := server.ListenAndServe()
+	go func() {
+		slog.Info("server starting", "port", server.Addr)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			slog.Error("server failed to start", "error", err)
+			os.Exit(1)
+		}
+	}()
 
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Println("Server closed")
-	} else if err != nil {
-		fmt.Printf("Error starting server: %s\n", err)
+	// set up gracefull shotdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-quit
+	slog.Info("shutting down server", "signal", sig.String())
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("server forced to shutdown", "error", err)
 		os.Exit(1)
 	}
+
+	slog.Info("server stopped gracefully")
 }
