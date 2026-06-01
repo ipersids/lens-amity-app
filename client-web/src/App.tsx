45f5fe4640dc029e11d019dfcdbe36fc08f404d0
Julia Llama <julia.persidskaia@gmail.com>
2026-06-01 13:25:19 +0300

    chore(web): install tailwindcss

--- client-web/src/App.tsx
+++ client-web/src/App.tsx
@@ -1,9 +1,9 @@
-import "./App.css";
-
 function App() {
   return (
-    <section id="center">
-      <p>Hello</p>
+    <section id="container" className="flex justify-center">
+      <div className="w-75 bg-white dark:bg-black rounded-xl shadow-md overflow-hidden">
+        <h1 className="text-lg dark:text-white font-bold p-2">Project Title</h1>
+      </div>
     </section>
   );
 }
